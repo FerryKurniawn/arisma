e49d90e39557a6fb15daec51d9d1d97b33e5738b
Ferry Kurniawan <ferrysavvy@gmail.com>
2025-04-28 11:48:25 +0700

    menambahkan endpoint untuk melihat dan membuat user

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,3 +17,44 @@ app.listen(PORT, (err) => {
 app.get("/api", (req, res) => {
   res.send("Welcome to my api boskuh");
 });
+
+app.get("/api/user", async (req, res) => {
+  const user = await prisma.user.findMany();
+  res.send(user);
+});
+
+app.post("/api/register", async (req, res) => {
+  const { username, password, role } = req.body;
+
+  try {
+    const existingUser = await prisma.user.findUnique({ where: { username } });
+    if (existingUser) {
+      return res.status(400).json({ message: "Username sudah digunakan" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+        role: role.toUpperCase(), // Misal: "ADMIN", "KEPALA_SEKOLAH"
+      },
+    });
+
+    res.status(201).json({
+      message: "User berhasil terdaftar",
+      user: {
+        id: newUser.id,
+        username: newUser.username,
+        role: newUser.role,
+      },
+    });
+  } catch (err) {
+    console.error("Register error:", err);
+    res.status(500).json({
+      message: "Terjadi kesalahan server",
+      error: err.message,
+    });
+  }
+});
